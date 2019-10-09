e56164cb2ca3029f25dd17f406cfbad8c0cf95d5
Jon Caneiro <joncaneiro@gmail.com>
2019-10-09 11:27:22 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+class Cat 
+  
+  def name 
+    @name 
+  end
+  
+  def name 
+    @name
+  end
+  
+  def meow
+    @meow
+  end
+
+  def meow
+  @meow = meow
+  end
+  
+end
